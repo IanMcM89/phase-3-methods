cc181b0cffbd9488cb38306101008d09734f55b1
Ian <ianmcm.89@gmail.com>
2022-01-18 12:34:10 -0500

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,30 @@
 # Your code here!
+
+#greet_programmer
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+#greet
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+#greet_with_default
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+#add
+def add(num1, num2)
+    num1 + num2
+end
+
+#halve
+def halve(number)
+    if number.class != Integer
+        return nil
+    end
+
+    number / 2
+end
